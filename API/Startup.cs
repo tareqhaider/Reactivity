fd19a7046b86916a8c5c5bc67e187b2ec21a9b6b
tareqhaider <muhammad.tareq.haider@gmail.com>
2020-05-09 14:35:47 +0600

    client-app initial

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,47 +13,48 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
 
-namespace API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace API {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(opt => 
-            {
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<DataContext> (opt => {
 
-                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                opt.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection"));
 
             });
-            services.AddControllers();
-            //services.AddMvc();
+
+            services.AddCors (opt => {
+                opt.AddPolicy ("CorsPolicy", policy => {
+                    policy.AllowAnyHeader ().AllowAnyMethod ().WithOrigins ("http://localhost:3000");
+                });
+            });
+
+            services.AddControllers ();
+            services.AddMvc ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
             //app.UseHttpsRedirection();
+            //app.UseMvc ();
+            
+            app.UseRouting ();
+            app.UseCors("CorsPolicy");
 
-            app.UseRouting();
+            app.UseAuthorization ();
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
